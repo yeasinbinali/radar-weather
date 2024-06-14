0a1b664e31be3cb7da2acafefd6d1902db8247e3
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-14 11:10:16 +0600

    get weather data using axios

--- src/components/SearchFunction/SearchFunction.jsx
+++ src/components/SearchFunction/SearchFunction.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form"
 
-const SearchFunction = () => {
+const SearchFunction = ({ onSearch }) => {
     const { register, handleSubmit } = useForm();
+    // const [city, setCity] = useState('');
+
     const onSubmit = (data) => {
-        console.log(data)
+        const city = data.search;
+        console.log(city)
+        if (city) {
+            onSearch(city);
+        }
     }
+
     return (
         <div className='my-5'>
             <form className='text-center' onSubmit={handleSubmit(onSubmit)}>
                 <input className='border-[1px] border-black p-1 w-[50%]' placeholder='Search your city' {...register("search")} />
-                <input className='border-[1px] border-black p-1' type="submit" />
+                <input className='border-[1px] border-black p-1 cursor-pointer' type="submit" />
             </form>
         </div>
     );
--- src/components/WeatherDashboard/WeatherDashboard.jsx
+++ src/components/WeatherDashboard/WeatherDashboard.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchFunction from '../SearchFunction/SearchFunction';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const WeatherDashboard = () => {
+    const [currentCity, setCurrentCity] = useState('');
+    const api_key = `9d4f183c0f4904ffa238af96cafbb11d`;
+
+    const weatherData = async (city) => {
+        try {
+            const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
+            const forecastWeatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=metric`
+
+            const currentWeatherResponse = await axios.get(currentWeatherURL);
+            console.log(currentWeatherResponse);
+        }
+        catch (error) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Something went wrong!",
+                footer: `${error.message}`
+            });
+        }
+    }
+
+    const handleSearch = (city) => {
+        setCurrentCity(city);
+        weatherData(city);
+    }
+
     return (
         <div>
-            <SearchFunction></SearchFunction>
+            <SearchFunction
+                onSearch={handleSearch}
+            ></SearchFunction>
         </div>
     );
 };
